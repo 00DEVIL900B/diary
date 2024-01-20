fdcdfdb545ea8b705e6ccad2fd1c1b1fc982930f
00DEVIL900B <lileshwarmessi@gmail.com>
2024-01-20 08:47:45 +0530

    Initial project

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo "BUILD START"
-python 3.9 -m install -r requirements.txt
-python 3.9 manage.py collectstatic --noinput --clear
+python -m install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+python manage.py runserver
+
 echo "BUILD END"

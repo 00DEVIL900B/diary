f71a06ec39e6911d001ab0a59117ba88e9dcbc53
00DEVIL900B <lileshwarmessi@gmail.com>
2024-01-20 09:00:01 +0530

    Changes

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
-echo "BUILD START"
-python -m install -r requirements.txt
-python manage.py collectstatic --noinput --clear
-python manage.py runserver
-
-echo "BUILD END"
+#!/bin/bash
+python 3.9 manage.py migrate
+python 3.9 manage.py collectstatic --noinput
+python 3.9 manage.py runserver 0.0.0.0:$PORT
